c6289808de126614c7451367d1196577187fbf8c
farrar142 <gksdjf1690@gmail.com>
2024-11-15 10:50:27 +0900

    feature: QuestionRepository에 subject로 Question을찾는 메서드 추가

--- src/main/kotlin/com/site/sbb/QuestionRepository.kt
+++ src/main/kotlin/com/site/sbb/QuestionRepository.kt
@@ -3,4 +3,5 @@ package com.site.sbb
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionRepository:JpaRepository<Question,Int>   {
+    fun findBySubject(subject:String):Question
 }
