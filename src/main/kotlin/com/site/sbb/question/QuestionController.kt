467bf3c9eae0ed71303ba7364ef37dafbbacd47e
farrar142 <gksdjf1690@gmail.com>
2024-11-15 13:40:52 +0900

    feature: 컨트롤러의 모델에 questionlist 추가

--- src/main/kotlin/com/site/sbb/question/QuestionController.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionController.kt
@@ -1,14 +1,19 @@
 package com.site.sbb.question
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 class QuestionController {
+    @Autowired
+    lateinit var questionRepository: QuestionRepository
     @GetMapping("/question/list")
-    fun list():String{
+    fun list(model:Model):String{
+        val questionList = this.questionRepository.findAll()
+        model.addAttribute("questionList",questionList)
         return "question_list"
     }
 }
