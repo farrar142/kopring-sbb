392009034729a27ee6b151106030a24d6ecf385a
farrar142 <gksdjf1690@gmail.com>
2024-11-15 10:58:05 +0900

    fix: 리포지터리 메서드 수정

--- src/main/kotlin/com/site/sbb/QuestionRepository.kt
+++ src/main/kotlin/com/site/sbb/QuestionRepository.kt
@@ -1,6 +1,7 @@
 package com.site.sbb
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface QuestionRepository:JpaRepository<Question,Int>   {
     fun findBySubject(subject:String):Question
