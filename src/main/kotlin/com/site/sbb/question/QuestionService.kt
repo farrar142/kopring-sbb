c671ac7cafc5a0f0019659dce91030f4edbce5ca
farrar142 <gksdjf1690@gmail.com>
2024-11-15 15:08:53 +0900

    feature: id로 question을 가져오는 함수 추가

--- src/main/kotlin/com/site/sbb/question/QuestionService.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionService.kt
@@ -1,6 +1,8 @@
 package com.site.sbb.question
 
+import com.site.sbb.DataNotFoundException
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 
 @Service
@@ -9,5 +11,11 @@ class QuestionService(
 ) {
     fun getList():List<Question>{
         return this.questionRepository.findAll()
+    }
+
+    fun getQuestion(id:Int):Question{
+        val qr =  this.questionRepository.findById(id)
+        if (qr.isPresent) return qr.get()
+        throw DataNotFoundException("question not found")
     }
 }
