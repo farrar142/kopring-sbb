940208febb4ee0d3db77d7a60b935b3397104ddf
farrar142 <gksdjf1690@gmail.com>
2024-11-15 16:13:14 +0900

    fix: 컨트롤러에 URL 프리픽스 적용

--- src/main/kotlin/com/site/sbb/question/QuestionController.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionController.kt
@@ -6,20 +6,22 @@ import org.springframework.stereotype.Service
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
 
 @Controller
+@RequestMapping("/question")
 class QuestionController(
     val questionService: QuestionService
 ) {
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     fun list(model:Model):String{
         val questionList = this.questionService.getList()
         model.addAttribute("questionList",questionList)
         return "question_list"
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     fun detail(model :Model, @PathVariable("id") id : Int):String{
         val q = this.questionService.getQuestion(id)
         model.addAttribute("question",q)
