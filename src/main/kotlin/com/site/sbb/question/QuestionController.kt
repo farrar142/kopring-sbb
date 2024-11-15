f85d76cacf601be1d85ea591dc120b20b553bd76
farrar142 <gksdjf1690@gmail.com>
2024-11-15 14:59:37 +0900

    feature: 컨트롤러에 디테일 경로 추가

--- src/main/kotlin/com/site/sbb/question/QuestionController.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionController.kt
@@ -5,16 +5,23 @@ import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 
 @Controller
-class QuestionController {
-    @Autowired
-    lateinit var questionService: QuestionService
+class QuestionController(
+    val questionService: QuestionService
+) {
     @GetMapping("/question/list")
     fun list(model:Model):String{
         val questionList = this.questionService.getList()
         model.addAttribute("questionList",questionList)
         return "question_list"
     }
+
+    @GetMapping("/question/detail/{id}")
+    fun detail(model :Model, @PathVariable("id") id : Int):String{
+        return "question_detail"
+    }
+
 }
