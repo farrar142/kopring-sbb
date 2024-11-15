1cd4d39232f0bc5fcbfd39687a04cacd7496cf38
farrar142 <gksdjf1690@gmail.com>
2024-11-15 14:32:13 +0900

    fix: Question컨트롤러에서 repository가아닌 service를 호출

--- src/main/kotlin/com/site/sbb/question/QuestionController.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionController.kt
@@ -2,6 +2,7 @@ package com.site.sbb.question
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class QuestionController {
     @Autowired
-    lateinit var questionRepository: QuestionRepository
+    lateinit var questionService: QuestionService
     @GetMapping("/question/list")
     fun list(model:Model):String{
-        val questionList = this.questionRepository.findAll()
+        val questionList = this.questionService.getList()
         model.addAttribute("questionList",questionList)
         return "question_list"
     }
