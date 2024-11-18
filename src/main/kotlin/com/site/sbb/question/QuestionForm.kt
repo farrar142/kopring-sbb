2d76595c091fd7f023fddcfeb53ed66846ca3d0f
farrar142 <gksdjf1690@gmail.com>
2024-11-18 16:06:57 +0900

    fix: QuestionForm수정

--- src/main/kotlin/com/site/sbb/question/QuestionForm.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionForm.kt
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Size
 
 class QuestionForm{
-    @NotEmpty(message="제목은 필수항목입니다.")
-    @Size(max=200)
+    @field:NotEmpty(message="제목은 필수항목입니다.")
+    @field:Size(max=200)
     var subject: String=""
 
-    @NotEmpty(message="내용은 필수항목입니다.")
+    @field:NotEmpty(message="내용은 필수항목입니다.")
     var content: String=""
 }
