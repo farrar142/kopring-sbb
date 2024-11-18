5dd7d2dc462ad7de111b37e904b144fd50a31c4b
farrar142 <gksdjf1690@gmail.com>
2024-11-18 15:18:05 +0900

    chore: 스프링부트 밸리데이션 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	//spring validation
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
