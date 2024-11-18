2cea54ab79822515bcf653d50fc9b1e3dcb21662
farrar142 <gksdjf1690@gmail.com>
2024-11-18 15:34:59 +0900

    fix: AnswerController수정

--- src/main/kotlin/com/site/sbb/answer/AnswerController.kt
+++ src/main/kotlin/com/site/sbb/answer/AnswerController.kt
@@ -3,6 +3,7 @@ package com.site.sbb.answer
 import com.site.sbb.question.QuestionService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,9 +15,13 @@ import org.springframework.web.bind.annotation.RequestParam
 class AnswerController(val questionService:QuestionService,val answerService: AnswerService) {
 
     @PostMapping("/create/{id}")
-    fun createAnswer(model:Model,@PathVariable("id") id:Int,@RequestParam(value="content") content:String):String{
+    fun createAnswer(model:Model,@PathVariable("id") id:Int,answerForm:AnswerForm,bindingResult: BindingResult):String{
         val q = this.questionService.getQuestion(id)
-        this.answerService.create(q,content)
+        if (bindingResult.hasErrors()){
+            model.addAttribute("question",q)
+            return "question_detail"
+        }
+        this.answerService.create(q,answerForm.content)
         return String.format("redirect:/question/detail/%s",id)
     }
 }
