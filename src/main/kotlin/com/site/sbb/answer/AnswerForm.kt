2b5d8cc2207e8422bfd86895a8fd82902c05d41f
farrar142 <gksdjf1690@gmail.com>
2024-11-18 16:05:06 +0900

    feature: 오류메세지 템플릿 추가

--- src/main/kotlin/com/site/sbb/answer/AnswerForm.kt
+++ src/main/kotlin/com/site/sbb/answer/AnswerForm.kt
@@ -3,7 +3,7 @@ package com.site.sbb.answer
 import jakarta.validation.constraints.NotEmpty
 
 
-data class AnswerForm (
+class AnswerForm (
     @field:NotEmpty(message="내용은 필수입니다.")
     val content:String=""){
 }
