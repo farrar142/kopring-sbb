fcaac2d6827abbab3ffb293de7564830347964b1
farrar142 <gksdjf1690@gmail.com>
2024-11-19 11:52:20 +0900

    fix: form에러 수정

--- src/main/kotlin/com/site/sbb/user/UserController.kt
+++ src/main/kotlin/com/site/sbb/user/UserController.kt
@@ -19,7 +19,8 @@ class UserController (val userService: UserService){
     fun signup(@Valid userCreateForm: UserCreateForm, bindingResult:BindingResult):String{
         if (bindingResult.hasErrors())return "signup_form"
         if (userCreateForm.password1!=userCreateForm.password2){
-            bindingResult.rejectValue("password2","passwordInCorrect")
+            println("password not matched")
+            bindingResult.rejectValue("password2","passwordInCorrect","2개의 패스워드가 일치하지 않습니다.")
             return "signup_form"
         }
         userService.create(userCreateForm.username,userCreateForm.email,userCreateForm.password1)
--- src/main/kotlin/com/site/sbb/user/UserCreateForm.kt
+++ src/main/kotlin/com/site/sbb/user/UserCreateForm.kt
@@ -4,18 +4,16 @@ import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Size
 
-class UserCreateForm {
+class UserCreateForm(
     @field:Size(min=3,max=25)
     @field:NotEmpty(message="사용자ID는 필수항목입니다.")
-    var username:String=""
+    val username:String="",
 
     @field:NotEmpty(message="비밀번호는 필수항목입니다.")
-    var password1:String = ""
-
+    val password1:String = "",
     @field:NotEmpty(message="비밀번호 확인은 필수항목입니다.")
-    var password2:String=""
-
+    val password2:String="",
     @field:NotEmpty(message="이메일은 필수항목입니다.")
     @field:Email
-    var email:String=""
-}
+    val email:String=""
+    )
