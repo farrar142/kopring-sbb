73704728ee3495ef7080ea4e3b9d9d2d4439245f
farrar142 <gksdjf1690@gmail.com>
2024-11-19 12:40:10 +0900

    feature: 시큐리티 bean에 authentication매니저 등록

--- src/main/kotlin/com/site/sbb/SecurityConfig.kt
+++ src/main/kotlin/com/site/sbb/SecurityConfig.kt
@@ -2,6 +2,8 @@ package com.site.sbb
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -33,5 +35,9 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder():PasswordEncoder{
         return BCryptPasswordEncoder()
+    }
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration):AuthenticationManager{
+        return authenticationConfiguration.authenticationManager
     }
 }
