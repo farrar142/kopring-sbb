f24380946c7e481f4e541f44142d02b36bdf7c45
farrar142 <gksdjf1690@gmail.com>
2024-11-19 11:13:57 +0900

    feature: passwordEncoder를 Bean에서 주입된 객체ë로 사용

--- src/main/kotlin/com/site/sbb/user/UserService.kt
+++ src/main/kotlin/com/site/sbb/user/UserService.kt
@@ -1,15 +1,15 @@
 package com.site.sbb.user
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserService(val userRepository: UserRepository) {
+class UserService(val userRepository: UserRepository,val passwordEncoder: PasswordEncoder) {
     fun create(username:String,email:String,password:String):SiteUser{
         val u = SiteUser()
         u.username = username
         u.email = email
-        val passwordEncoder =  BCryptPasswordEncoder()
         val encodedPassword = passwordEncoder.encode(password)
         u.password = encodedPassword
         userRepository.save(u)
