38d6ee00ccc0e2be43fcdba35bf9801f46980981
farrar142 <gksdjf1690@gmail.com>
2024-11-20 10:22:08 +0900

    feature: Answer컨트롤러와 서비스에서 author를 받도ë¡

--- src/main/kotlin/com/site/sbb/answer/AnswerController.kt
+++ src/main/kotlin/com/site/sbb/answer/AnswerController.kt
@@ -1,6 +1,7 @@
 package com.site.sbb.answer
 
 import com.site.sbb.question.QuestionService
+import com.site.sbb.user.UserService
 import jakarta.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -9,20 +10,26 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.security.Principal
 
 
 @Controller
 @RequestMapping("/answer")
-class AnswerController(val questionService:QuestionService,val answerService: AnswerService) {
+class AnswerController(
+    val questionService:QuestionService,
+    val answerService: AnswerService,
+    val userService:UserService
+    ) {
 
     @PostMapping("/create/{id}")
-    fun createAnswer(model:Model, @PathVariable("id") id:Int, @Valid answerForm:AnswerForm, bindingResult: BindingResult):String{
-        val q = this.questionService.getQuestion(id)
+    fun createAnswer(model:Model, @PathVariable("id") id:Int, @Valid answerForm:AnswerForm, bindingResult: BindingResult,principal: Principal):String{
+        val u = userService.getUser(principal.name)
+        val q = questionService.getQuestion(id)
         if (bindingResult.hasErrors()){
             model.addAttribute("question",q)
             return "question_detail"
         }
-        this.answerService.create(q,answerForm.content)
+        this.answerService.create(q,answerForm.content,u)
         return String.format("redirect:/question/detail/%s",id)
     }
 }
--- src/main/kotlin/com/site/sbb/answer/AnswerService.kt
+++ src/main/kotlin/com/site/sbb/answer/AnswerService.kt
@@ -2,6 +2,7 @@ package com.site.sbb.answer
 
 import com.site.sbb.question.Question
 import com.site.sbb.question.QuestionService
+import com.site.sbb.user.SiteUser
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -9,10 +10,11 @@ import java.time.LocalDateTime
 class AnswerService(
     val answerRepository: AnswerRepository
 ){
-    fun create(question:Question,content:String){
+    fun create(question:Question,content:String,author:SiteUser){
         val a = Answer()
         a.question=question
         a.content=content
+        a.author=author
         a.createDate = LocalDateTime.now()
         this.answerRepository.save(a)
     }
