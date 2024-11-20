02d4ae68a2de11f3864554ccc9eaca7d443030f4
farrar142 <gksdjf1690@gmail.com>
2024-11-20 10:18:21 +0900

    feature: UserController에 유저의 이름으로 유저를 반환하는 메서드 추가

--- src/main/kotlin/com/site/sbb/user/UserService.kt
+++ src/main/kotlin/com/site/sbb/user/UserService.kt
@@ -1,11 +1,18 @@
 package com.site.sbb.user
 
+import com.site.sbb.DataNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class UserService(val userRepository: UserRepository,val passwordEncoder: PasswordEncoder) {
+    fun getUser(username:String):SiteUser{
+        val siteUser:Optional<SiteUser> = userRepository.findByusername(username)
+        if (siteUser.isPresent)return siteUser.get()
+        throw DataNotFoundException("siteuser not found")
+    }
     fun create(username:String,email:String,password:String):SiteUser{
         val u = SiteUser()
         u.username = username
