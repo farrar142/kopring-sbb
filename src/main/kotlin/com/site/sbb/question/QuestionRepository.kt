e3b4a7388b482112cf777f1ab54a03562fb9ec19
farrar142 <gksdjf1690@gmail.com>
2024-11-22 14:04:07 +0900

    feature: QuestionController에 specficiation과 pageable을 인수로받는 메서드 추가

--- src/main/kotlin/com/site/sbb/question/QuestionRepository.kt
+++ src/main/kotlin/com/site/sbb/question/QuestionRepository.kt
@@ -3,6 +3,7 @@ package com.site.sbb.question
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionRepository:JpaRepository<Question,Int>   {
@@ -10,4 +11,5 @@ interface QuestionRepository:JpaRepository<Question,Int>   {
     fun findBySubjectAndContent(subject:String,content:String): Question
     fun findBySubjectLike(subject:String):List<Question>
     override fun findAll(pageable:Pageable):Page<Question>
+    fun findAll(spec:Specification<Question>,pageable: Pageable)
 }
