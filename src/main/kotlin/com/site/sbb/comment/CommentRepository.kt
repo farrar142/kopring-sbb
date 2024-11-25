9411a30deaf7cbe55ccfe186174154821e277c4a
farrar142 <gksdjf1690@gmail.com>
2024-11-25 10:44:54 +0900

    feature: Comment리포지터리추가

---
+++ src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
@@ -0,0 +1,10 @@
+package com.site.sbb.comment
+
+import com.site.sbb.answer.Answer
+import com.site.sbb.question.Question
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentRepository :JpaRepository<Comment,Int> {
+    fun findByQuestion(question:Question)
+    fun findByAnswer(answer:Answer)
+}
