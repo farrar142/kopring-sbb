be8e197f0680d3ac11e9226ea0176a336c03f20d
farrar142 <gksdjf1690@gmail.com>
2024-11-25 11:23:07 +0900

    fix: CommentRepository의 반환 타입 수정

--- src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
+++ src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
@@ -5,6 +5,6 @@ import com.site.sbb.question.Question
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository :JpaRepository<Comment,Int> {
-    fun findByQuestion(question:Question):List<Question>
-    fun findByAnswer(answer:Answer):List<Question>
+    fun findByQuestion(question:Question):List<Comment>
+    fun findByAnswer(answer:Answer):List<Comment>
 }
