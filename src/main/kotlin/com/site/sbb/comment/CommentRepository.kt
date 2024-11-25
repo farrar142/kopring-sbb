2652dcfc89ef924133c23f92eb570cedc502d07e
farrar142 <gksdjf1690@gmail.com>
2024-11-25 11:26:31 +0900

    fix: import 수정

--- src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
+++ src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
@@ -4,11 +4,11 @@ import com.site.sbb.answer.Answer
 import com.site.sbb.question.Question
 import com.site.sbb.user.SiteUser
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import java.awt.print.Pageable
 
 interface CommentRepository :JpaRepository<Comment,Int> {
     fun findByQuestion(question:Question):List<Comment>
     fun findByAnswer(answer:Answer):List<Comment>
-    fun findByAuthor(author:SiteUser,pageable:Pageable):Page<Comment>
+    fun findByAuthor(author:SiteUser,pageable: Pageable):Page<Comment>
 }
