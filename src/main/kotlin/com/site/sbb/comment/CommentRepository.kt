5f23304c837cbf063d110afb9a2ae5e312935721
farrar142 <gksdjf1690@gmail.com>
2024-11-25 11:25:42 +0900

    feature: Author의 코멘트리스트들을 가져오는 메서드 추가

--- src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
+++ src/main/kotlin/com/site/sbb/comment/CommentRepository.kt
@@ -2,9 +2,13 @@ package com.site.sbb.comment
 
 import com.site.sbb.answer.Answer
 import com.site.sbb.question.Question
+import com.site.sbb.user.SiteUser
+import org.springframework.data.domain.Page
 import org.springframework.data.jpa.repository.JpaRepository
+import java.awt.print.Pageable
 
 interface CommentRepository :JpaRepository<Comment,Int> {
     fun findByQuestion(question:Question):List<Comment>
     fun findByAnswer(answer:Answer):List<Comment>
+    fun findByAuthor(author:SiteUser,pageable:Pageable):Page<Comment>
 }
