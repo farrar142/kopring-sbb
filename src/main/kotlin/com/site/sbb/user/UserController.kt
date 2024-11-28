75018dd438946d25e4b9b3a127c00bcac1136ab1
farrar142 <gksdjf1690@gmail.com>
2024-11-28 11:34:00 +0900

    fix: user에서 인증관련 로직을 auth로 수정

--- src/main/kotlin/com/site/sbb/user/UserController.kt
+++ src/main/kotlin/com/site/sbb/user/UserController.kt
@@ -23,55 +23,10 @@ import java.util.*
 @RequestMapping("/user")
 class UserController (
                       val userService: UserService,
-                      val mailSender:MailSender,
                       val categoryService: CategoryService,
                       val questionService: QuestionService,
                       val answerService: AnswerService,
                       val commentService: CommentService){
-    @GetMapping("/signup")
-    fun signup(userCreateForm: UserCreateForm):String{
-        return "signup_form"
-    }
-
-    @PostMapping("/signup")
-    fun signup(@Valid userCreateForm: UserCreateForm, bindingResult:BindingResult):String{
-        if (bindingResult.hasErrors())return "signup_form"
-        if (userCreateForm.password1!=userCreateForm.password2){
-            println("password not matched")
-            bindingResult.rejectValue("password2","passwordInCorrect","2개의 패스워드가 일치하지 않습니다.")
-            return "signup_form"
-        }
-        try{
-            userService.create(userCreateForm.username,userCreateForm.email,userCreateForm.password1)
-        }catch (error:DataIntegrityViolationException){
-            error.printStackTrace()
-            bindingResult.reject("signupFailed","이미 등록된 사용자입니다.")
-            return "signup_form"
-        }catch (error:Exception){
-            error.printStackTrace()
-            bindingResult.reject("signupFailed",error.message.toString())
-            return "signup_form"
-        }
-        return "redirect:/"
-    }
-
-    private fun setContents(model: Model,user: SiteUser,
-                            qPage:Int,aPage:Int,cPage:Int){
-        val categoryList = categoryService.getList()
-        val questionList =questionService.getListByAuthor(user,qPage)
-        val answerList = answerService.getListByAuthor(user,aPage)
-        val commentList = commentService.getListByAuthor(user,cPage)
-
-        model.addAttribute("categoryList", categoryList);
-        model.addAttribute("questionPaging", questionList)
-        model.addAttribute("answerPaging", answerList)
-        model.addAttribute("commentPaging", commentList)
-        model.addAttribute("user", user)
-        model.addAttribute("qPage",qPage)
-        model.addAttribute("aPage",aPage)
-        model.addAttribute("cPage",cPage)
-    }
-
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
     fun profile(model:Model,
@@ -107,6 +62,23 @@ class UserController (
         return "profile_detail"
     }
 
+    private fun setContents(model: Model, user: SiteUser,
+                            qPage:Int, aPage:Int, cPage:Int){
+        val categoryList = categoryService.getList()
+        val questionList =questionService.getListByAuthor(user,qPage)
+        val answerList = answerService.getListByAuthor(user,aPage)
+        val commentList = commentService.getListByAuthor(user,cPage)
+
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("questionPaging", questionList)
+        model.addAttribute("answerPaging", answerList)
+        model.addAttribute("commentPaging", commentList)
+        model.addAttribute("user", user)
+        model.addAttribute("qPage",qPage)
+        model.addAttribute("aPage",aPage)
+        model.addAttribute("cPage",cPage)
+    }
+
     @GetMapping("/reset_password")
     fun resetPassword(model: Model):String{
         model.addAttribute("error",false)
@@ -115,38 +87,4 @@ class UserController (
         return "reset_password"
     }
 
-    @PostMapping("/reset_password")
-    fun resetPassword(model: Model,
-                      @RequestParam(value="email") email:String):String{
-        var error = false
-        var sendConfirm = true
-        try{
-            val user = userService.getUserByEmail(email)
-            val simpleMailMessage = SimpleMailMessage()
-            simpleMailMessage.setTo(email)
-            simpleMailMessage.subject="계정 정보입니다."
-            val newPassword = UUID.randomUUID().toString().replace("-","")
-            val sb = StringBuilder().run{
-                append(user.username)
-                append("계정의 비밀번호를 새롭게 초기화 했습니다.\n")
-                append("새 비밀번호는 ").append(newPassword).append(" 입니다.\n")
-                append("로그인 후 내 정보에서 새로 비밀번호를 지정해주세요")
-            }
-            simpleMailMessage.text = sb.toString()
-            userService.updatePassword(user,newPassword)
-            Thread{mailSender.send(simpleMailMessage)}.start()
-        }catch (e:Exception) {
-            error = true
-            sendConfirm = false
-        }
-        model.addAttribute("error", error)
-        model.addAttribute("sendConfirm", sendConfirm)
-        model.addAttribute("email", email)
-        return "reset_password"
-    }
-
-    @GetMapping("/login")
-    fun login():String{
-        return "login_form"
-    }
 }
